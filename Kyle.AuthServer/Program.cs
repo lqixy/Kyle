077fd2d122e9c74a3286845fb2a7207352a90835
liuqi@tongtongmall.com <liuqi@tongtongmall.com>
2023-02-24 10:23:46 +0800

    自定义密码验证: ResourceOwnerPasswordValidator 加载自定义声明: ProfileService

--- Kyle.AuthServer/Program.cs
+++ Kyle.AuthServer/Program.cs
@@ -1,10 +1,13 @@
+using Kyle.AuthServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServer()
     .AddInMemoryApiScopes(builder.Configuration.GetSection("IdentityServer:ApiScopes"))
     .AddInMemoryApiResources(builder.Configuration.GetSection("IdentityServer:ApiResources"))
     .AddInMemoryClients(builder.Configuration.GetSection("IdentityServer:Clients"))
-
+    .AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
+    .AddProfileService<ProfileService>()
     ;
 
 var app = builder.Build();
---
+++ Kyle.AuthServer/Validators/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System.Security.Claims;
+using static IdentityModel.OidcConstants;
+
+namespace Kyle.AuthServer
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var userName = context.UserName;
+            var password = context.Password;
+            if (userName == null || password == null)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "账号或密码错误");
+            }
+            else
+            {
+                context.Result = new GrantValidationResult(Guid.NewGuid().ToString(), AuthenticationMethods.Password,
+                    new Claim[]
+                    {
+                        new Claim("tenantId",Guid.NewGuid().ToString()),
+                        new Claim(JwtClaimTypes.Role,"admin"),
+                        new Claim(JwtClaimTypes.Name,userName),
+                    });
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
