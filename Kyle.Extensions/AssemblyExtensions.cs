490e6ef5d3b5ff1864dddc060c6e7a868ba67743
liuqi@tongtongmall.com <liuqi@tongtongmall.com>
2023-02-24 14:38:18 +0800

    添加扩展方法

---
+++ Kyle.Extensions/AssemblyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyle.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static Assembly[] GetAssemblies()
+        {
+            return DependencyContext.Default.CompileLibraries
+                .Where(x => !x.Serviceable && x.Type != "package")
+                .Select(x => AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(x.Name))).ToArray();
+        }
+    }
+}
