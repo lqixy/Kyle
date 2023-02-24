de8b6247c7c668a1f422013dcb1ef6022b40b381
liuqi@tongtongmall.com <liuqi@tongtongmall.com>
2023-02-24 10:46:56 +0800

    authserver增加证书

--- Kyle.AuthServer/Program.cs
+++ Kyle.AuthServer/Program.cs
@@ -3,6 +3,7 @@ using Kyle.AuthServer;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServer()
+    .AddDeveloperSigningCredential()
     .AddInMemoryApiScopes(builder.Configuration.GetSection("IdentityServer:ApiScopes"))
     .AddInMemoryApiResources(builder.Configuration.GetSection("IdentityServer:ApiResources"))
     .AddInMemoryClients(builder.Configuration.GetSection("IdentityServer:Clients"))
